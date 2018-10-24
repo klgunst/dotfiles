8425d418232d91b19a9bfd2e89535fb0ffc04eb7
Klaas Gunst <Klaas.Gunst@UGent.be>
2018-10-24 11:19:30 +0200

    Made some changes to otex
    Can also pass the directory as argument
    and included it in my i3config and my zshell aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,5 +5,4 @@ alias rcd='ranger --choosedir=/tmp/rcd; LASTDIR=`cat /tmp/rcd`; cd "$LASTDIR"'
 alias rm="rm -v "
 alias gpg="gpg2"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias paper="cd $HOME/Documents/papers/T3NS_SU2; vim T3NS_SU2.tex"
-
+alias paper="otex $HOME/Documents/papers/T3NS_SU2"
