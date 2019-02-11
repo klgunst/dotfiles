64aa756d71aa58f1596768d600565fb96a8b211d
Klaas Gunst <Klaas.Gunst@UGent.be>
2019-02-10 22:27:07 -0800

    changed neomutt for pass

--- .config/scripts/neomutt.sh
+++ .config/scripts/neomutt.sh
@@ -2,6 +2,5 @@
 
 cd ~/Downloads # location where to save attachments
 # prompts you to ask for passphrase
-gpg2 -dq ~/.config/offlineimap/ugent.gpg > /dev/null
-offlineimap > /dev/null 2>&1&
+pass Email/hotmail > /dev/null
 neomutt
