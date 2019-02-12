5c41d2546fb936ae76de6fe827f9dfb613358dd6
Klaas Gunst <Klaas.Gunst@UGent.be>
2019-02-12 13:08:05 -0800

    Some fixes mails

--- .config/scripts/neomutt.sh
+++ .config/scripts/neomutt.sh
@@ -3,4 +3,7 @@
 cd ~/Downloads # location where to save attachments
 # prompts you to ask for passphrase
 pass BigGuys/Windows > /dev/null
+offlineimap -a UGent &> /dev/null&
+offlineimap -a Hotmail &> /dev/null&
+offlineimap -a Caltech &> /dev/null&
 neomutt
