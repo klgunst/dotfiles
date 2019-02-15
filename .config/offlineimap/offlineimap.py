826d2b8c429977140dc15802a3e466c40573df0b
Klaas Gunst <Klaas.Gunst@UGent.be>
2019-02-15 11:20:27 -0800

    changed windows to microsoft

--- .config/offlineimap/offlineimap.py
+++ .config/offlineimap/offlineimap.py
@@ -5,7 +5,7 @@ def get_ugentpass():
         return check_output("pass work/UGent", shell=True).strip("\n")
 
 def get_hotmailpass():
-        return check_output("pass BigGuys/Windows | grep App-password | sed 's/App-password=//'", shell=True).strip("\n")
+        return check_output("pass BigGuys/Microsoft | grep App-password | sed 's/App-password=//'", shell=True).strip("\n")
 
 def get_caltechpass():
         return check_output("pass work/CalTech/access | head -n1", shell=True).strip("\n")
