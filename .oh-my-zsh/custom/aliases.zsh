b749ba29e62154d7237604121518387c1b8e0997
Klaas Gunst <klaasgunst@hotmail.com>
2019-09-25 13:46:10 +0200

    small changes zshrc

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -13,12 +13,26 @@ alias purvena='if [ "x$(pgrep vpnc)" = "x" ]; then startvpn; fi; startssh purven
 alias calana='if [ "x$(pgrep vpnc)" = "x" ]; then startvpn; fi; startssh calana'
 alias hpc="startssh hpc"
 alias pauling="startssh pauling"
+alias graham="startssh graham"
 alias padoi="papis add --from-doi"
+alias python=python3
 
 # search configs of my dotfile git repo
-se() { vim $HOME/$(git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --full-name ~ | fzf) ;}
+se() { 
+    file=$(git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --full-name ~ | fzf)
+    if [ ! -z "$file" ]; then
+	vim $HOME/$file
+    fi
+}
+
 # search code
-sec() { cd "$HOME/Codes/$(ls ~/Codes | fzf)" ;}
+sec() { 
+    dir=$(ls ~/Codes | fzf)
+    if [ ! -z "$dir" ]; then
+	cd $HOME/Codes/$dir
+    fi
+}
+
 follow() { watch "grep -E 'ENERGY|TRUNCATION|Total|DIM' $1 | tail -n8; echo ''; tail -n10 $1" }
 o() {
     vimopen=
